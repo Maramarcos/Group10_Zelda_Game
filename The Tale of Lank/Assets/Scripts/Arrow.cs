91c7f5f18e02bcc2d073a4c06da698ae22c42b7b
Ryan Beck <rjb17f@my.fsu.edu>
2021-11-27 16:01:46 -0500

    Lifetime Arrows
    Arrows now despawn after 10 Frames.

--- The Tale of Lank/Assets/Scripts/Arrow.cs
+++ The Tale of Lank/Assets/Scripts/Arrow.cs
@@ -6,10 +6,21 @@ public class Arrow : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D hitbox;
+    public float lifetime;
+    private float lifetimeCounter;
     // Start is called before the first frame update
     void Start()
     {
-        
+        lifetimeCounter = lifetime;
+    }
+
+    private void Update()
+    {
+        lifetimeCounter -= Time.deltaTime;
+        if (lifetimeCounter <= 0)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     public void Setup(Vector2 velocity, Vector3 direction)
