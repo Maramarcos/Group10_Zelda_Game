15de997aa08e469933a3978562c172fae61e154b
Ryan Beck <rjb17f@my.fsu.edu>
2021-11-26 20:54:39 -0500

    Working HP System
    HP system is working. Also contains some breakable pot fragments

--- The Tale of Lank/Assets/Scripts/CameraMovement.cs
+++ The Tale of Lank/Assets/Scripts/CameraMovement.cs
@@ -7,20 +7,51 @@ public class CameraMovement : MonoBehaviour
 
     public Transform target;
     public float smoothing;
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
 
-    // Start is called before the first frame update
+
+    // Use this for initialization
     void Start()
     {
-        
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
     }
 
-    // Late Update is called lasted
-    void FixedUpdate()
+    // Update is called once per frame
+    void LateUpdate()
     {
-        if(transform.position != target.position)
+        if (transform.position != target.position)
         {
-            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
-            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+            Vector3 targetPosition = new Vector3(target.position.x,
+                                                 target.position.y,
+                                                 transform.position.z);
+            targetPosition.x = Mathf.Clamp(targetPosition.x,
+                                           minPosition.x,
+                                           maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y,
+                                           minPosition.y,
+                                           maxPosition.y);
+
+            transform.position = Vector3.Lerp(transform.position,
+                                             targetPosition, smoothing);
+            //transform.position = Vector3.Lerp(transform.position,
+            //                                 targetPosition, smoothing);
         }
     }
+
+    private Vector3 RoundPosition(Vector3 position)
+    {
+        float xOffset = position.x % .0625f;
+        if (xOffset != 0)
+        {
+            position.x -= xOffset;
+        }
+        float yOffset = position.y % .0625f;
+        if (yOffset != 0)
+        {
+            position.y -= yOffset;
+        }
+        return position;
+    }
 }
+
